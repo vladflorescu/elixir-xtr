a116d6ecdeedd04f68d3291d6198cec2a6f3532a
rails <exit>
2018-04-22 21:45:37 +0300

    normalize Command options

--- lib/xtr/command.ex
+++ lib/xtr/command.ex
@@ -11,20 +11,25 @@ defmodule Xtr.Command do
 
   @doc """
     Example
-    Xtr.Command.exec("query fmi | sort-by stargazers_count:desc | limit 5 | only name")
+    Xtr.Command.exec("query fmi | sort-by stargazers_count:desc | limit 5 | only name stars")
   """
   def exec(str) do
-    [command | options] = str |> String.split("|") |> Enum.map(&String.trim/1)
+    [command | options_strs] = str |> String.split("|") |> Enum.map(&String.trim/1)
     [command_name | command_val] = command |> String.split(~r/\s+/)
 
     try do
-      run_command({command_name, command_val, options})
+      options = Enum.map(options_strs, fn str ->
+        [name | values] = String.split(str, " ")
+        {name, values}
+      end)
+
+      run_command(command_name, command_val, options)
     rescue
       FunctionClauseError -> raise InvalidCommandError, command_name
     end
   end
 
-  defp run_command({"query", val, options}) do
+  defp run_command("query", val, options) do
     Enum.join(options, " ")
   end
 end
