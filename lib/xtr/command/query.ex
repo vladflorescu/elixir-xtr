f8ce2f912d64fadb4ee75ed55ce69b5c3eb1f580
xsylver <xsylver@gmail.com>
2018-04-23 21:53:03 +0300

    - implemented limit filter

--- lib/xtr/command/query.ex
+++ lib/xtr/command/query.ex
@@ -17,45 +17,23 @@ defmodule Xtr.Command.Query do
       |> Enum.map(&Poison.decode!(~s(#{&1})))
       |> List.flatten;
 
-#    files = ["dir2"] |> Enum.map(&Path.wildcard("./data/#{&1}/*.json")) |> Enum.map(&File.read!(&1)) |> Enum.map(&Poison.decode!(~s(#{&1}))) |> List.flatten;
-#    filters = ["id", "name"]
-#    options = [{"only", ["name"]}]
-#    query dir1 | only name
-
     #apply filters on files
+    IO.puts "Options = #{inspect options}";
     updatedFiles = Enum.reduce(options, files, fn({filter, values}, acc) -> applyFilter(filter, values, acc) end);
 
     "#{inspect updatedFiles}";
   end
 
   defp applyFilter("sort-by", filters, files) do
-    Logger.info("In sort-by method");
     Enum.reduce(filters, files, fn(filter, acc) -> Enum.sort_by(acc, fn mapp -> mapp[filter] end) end);
   end
 
   defp applyFilter("only", filters, files) do
-    Logger.info("In only method");
-    #keep only properties which are found in filters
-
-    Enum.map(
-        files,
-        fn file ->
-                Enum.reduce(
-                    file,
-                    %{},
-                    fn ({key, value}, acc) ->
-                       if(Enum.member?(filters, key)) do
-                         Map.put(acc, key, value)
-                       else
-                         acc
-                       end
-                    end)
-        end);
+    Enum.map(files, fn file -> Enum.reduce(file, %{}, fn ({key, value}, acc) -> if(Enum.member?(filters, key)) do  endMap.put(acc, key, value) else acc end end) end);
   end
 
   defp applyFilter("limit", [count | tail], files) do
-    Logger.info("In limit method!")
-    Enum.take(files, count);
+    Enum.take(files, elem(Integer.parse(count), 0));
   end
 
   defp applyFilter(invalidFilter, values, acc) do
